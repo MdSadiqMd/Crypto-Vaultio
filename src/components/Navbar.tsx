c7f65ca3296146aad42269bc132835ccaf449b33
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-10 22:56:51 +0530

    Feat: Changed Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box } from "lucide-react";
+import { Wallet } from "lucide-react";
 import React from "react";
 
 import { ModeToggle } from "./ui/theme-button";
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <nav className="flex justify-between items-center py-4">
             <div className="flex items-center gap-2">
-                <Box className="size-8" />
+                <Wallet className="size-8" />
                 <div className="flex flex-col gap-4">
                     <span className="tracking-tighter text-3xl font-extrabold text-primary flex gap-2 items-center">
                         Vault.io{" "}
